3a3e5f6b7c01d9a36d161191c011d23cedb9499c
FlaviaGarcia <flavi13a@gmail.com>
2020-02-10 20:16:53 +0100

    code cleaning

--- converge_and_attract.py
+++ converge_and_attract.py
@@ -5,126 +5,139 @@ Created on Sun Feb  9 10:31:13 2020
 @author: kwc57
 """
 
-# Import package dependencies
-import math
 import numpy as np 
 import matplotlib.pyplot as plt
 from itertools import permutations
+import hopfield_functions as hf
 
 
-# Initialize memory patterns
-x1 = np.array([-1., -1., 1., -1., 1., -1., -1., 1.]).reshape(1,8)
-x2 = np.array([-1., -1., -1., -1., -1., 1., -1., -1.]).reshape(1,8)
-x3 = np.array([-1., 1., 1., -1., -1., 1., -1., 1.]).reshape(1,8)
+
+def get_all_possible_patterns(): 
+    a0 = [ 1.,  1.,  1.,  1.,  1.,  1.,  1.,   1.]
+    a1 = [-1.,  1.,  1.,  1.,  1.,  1.,  1.,   1.]
+    a2 = [-1., -1.,  1.,  1.,  1.,  1.,  1.,   1.]
+    a3 = [-1., -1., -1.,  1.,  1.,  1.,  1.,   1.]
+    a4 = [-1., -1., -1., -1.,  1.,  1.,  1.,   1.]
+    a5 = [-1., -1., -1., -1., -1.,  1.,  1.,   1.]
+    a6 = [-1., -1., -1., -1., -1., -1.,  1.,   1.]
+    a7 = [-1., -1., -1., -1., -1., -1., -1.,   1.]
+    a8 = [-1., -1., -1., -1., -1., -1., -1.,   1.]
+    a9 = [-1., -1., -1., -1., -1., -1., -1.,  -1.] 
+    
+    perm0 = set(permutations(a0))
+    perm1 = set(permutations(a1))
+    perm2 = set(permutations(a2))
+    perm3 = set(permutations(a3))
+    perm4 = set(permutations(a4))
+    perm5 = set(permutations(a5))
+    perm6 = set(permutations(a6))
+    perm7 = set(permutations(a7))
+    perm8 = set(permutations(a8))
+    perm9 = set(permutations(a9))
+    
+    ar0 = np.array(list(perm0))
+    ar1 = np.array(list(perm1))
+    ar2 = np.array(list(perm2))
+    ar3 = np.array(list(perm3))
+    ar4 = np.array(list(perm4))
+    ar5 = np.array(list(perm5))
+    ar6 = np.array(list(perm6))
+    ar7 = np.array(list(perm7))
+    ar8 = np.array(list(perm8))
+    ar9 = np.array(list(perm9))
+    
+    all_pos = np.concatenate((ar0, ar1, ar2, ar3, ar4, ar5, ar6, ar7, ar8, ar9), axis=0)
+    
+    all_pos_unique = np.unique(all_pos, axis=0)
+    
+    return all_pos_unique
+
+
+# Memory patterns
+x1 = np.array([-1., -1., 1., -1., 1., -1., -1., 1.])
+x2 = np.array([-1., -1., -1., -1., -1., 1., -1., -1.])
+x3 = np.array([-1., 1., 1., -1., -1., 1., -1., 1.])
+
+
+patterns = np.array([x1, x2, x3])
 
 # Calculate Weight Matrix (Scaling optional)
-W = (1./8.)*(np.dot(x1.T,x1) + np.dot(x2.T,x2) + np.dot(x3.T,x3))
+#W = (1./8.)*(np.outer(x1,x1) + np.outer(x2,x2) + np.outer(x3,x3))
+W = hf.weight_calc(patterns, disp_W=True)
 
-# Display weight matrix as greyscale image
-plt.imshow(W,  cmap='gray')
-plt.show()
 
-#pict = np.loadtxt('pict.dat')
-#print(pict.shape)
-
+### Check that x1, x2, x3 are fixed points
+### Get xi based in precomputed W 
+"""
 x1_recall = np.sign(np.dot(W,x1.T).T)
 x2_recall = np.sign(np.dot(W,x2.T).T)
 x3_recall = np.sign(np.dot(W,x3.T).T)
+"""
 
-# Plot recalls over inputs to ensure proper weight matrix configuration
-#plt.title("Confirming proper x1 recall")
-#plt.plot(x1[0], label='Input')
-#plt.plot(x1_recall[0], label='Recall')
-#plt.legend()
-#plt.show()
-#
-#plt.title("Confirming proper x2 recall")
-#plt.plot(x2[0], label='Input')
-#plt.plot(x2_recall[0], label='Recall')
-#plt.legend()
-#plt.show()
-#
-#plt.title("Confirming proper x3 recall")
-#plt.plot(x3[0], label='Input')
-#plt.plot(x3_recall[0], label='Recall')
-#plt.legend()
-#plt.show()
+patterns_recall = hf.degraded_recall_first_to_converge(patterns, W)
+print("- Checking that x1, x2 and x3 are fixed points...")
+print(np.all(patterns==patterns_recall))
 
 
-# Initialize distorted memory patterns
-x1d = np.array([1., -1., 1., -1., 1., -1., -1., 1.]).reshape(1,8)
-x2d = np.array([1., 1., -1., -1., -1., 1., -1., -1.]).reshape(1,8)
-x3d = np.array([1., 1., 1., -1., 1., 1., -1., 1.]).reshape(1,8)
 
-for i in range(3):
-    x1d = np.sign(np.dot(W,x1d.T).T)
-    x2d = np.sign(np.dot(W,x2d.T).T)
-    x3d = np.sign(np.dot(W,x3d.T).T)
+# Distorted memory patterns
+x1d = np.array([1., -1., 1., -1., 1., -1., -1., 1.]) # 0 different
+x2d = np.array([1., 1., -1., -1., -1., 1., -1., -1.]) # 0, 1 different
+x3d = np.array([1., 1., 1., -1., 1., 1., -1., 1.]) # 0, 4 different
+
+patterns_distorted = np.array([x1d, x2d, x3d])
+
+patterns_distorted_recall = hf.degraded_recall_first_to_converge(patterns_distorted,W)
+
+
+
+patterns_distorted_recall = hf.degraded_recall_epochs(patterns_distorted, W, epochs=2000)
+
+
+np.unique(patterns_distorted_recall, axis=0)
+
+
+
+np.all(patterns_distorted_recall == patterns, axis=1)
+
+patterns_distorted_converg = np.all(patterns_distorted_recall == patterns, axis=1)
+
+print("Noisy patterns that converged: " )
+print(patterns_distorted_converg)
+
+
+
+# Plot recalls over distorted inputs to ensure proper 
+for idx_distorted_input in range(len(patterns_distorted_converg)):
+    txt_title = "Checking x" + str((idx_distorted_input+1)) + "d convergence"
+    plt.title(txt_title)
+    plt.plot(patterns[idx_distorted_input], "c", label='Attractor Input')
+    plt.plot(patterns_distorted_recall[idx_distorted_input],"--k", label='Distorted Recall')
+    plt.legend()
+    plt.show()
+
+
+all_possible_patterns = get_all_possible_patterns()
+
+
+all_possible_patterns_recall = hf.degraded_recall_epochs(all_possible_patterns, W, epochs=2000)
+
     
-# Plot recalls over inputs to ensure proper weight matrix configuration
-plt.title("Checking x1d convergence")
-plt.plot(x1[0], label='Attractor Input')
-plt.plot(x1d[0], label='Distorted Recall')
-plt.legend()
-plt.show()
-
-plt.title("Checking x2d convergence")
-plt.plot(x2[0], label='Attractor Input')
-plt.plot(x2d[0], label='Distorted Recall')
-plt.legend()
-plt.show()
-
-plt.title("Checking x3d convergence")
-plt.plot(x3[0], label='Attractor Input')
-plt.plot(x3d[0], label='Distorted Recall')
-plt.legend()
-plt.show()
-
-
-##### Automate the search for attractors ######
-a0 = [ 1.,  1.,  1.,  1.,  1.,  1.,  1.,   1.]
-a1 = [-1.,  1.,  1.,  1.,  1.,  1.,  1.,   1.]
-a2 = [-1., -1.,  1.,  1.,  1.,  1.,  1.,   1.]
-a3 = [-1., -1., -1.,  1.,  1.,  1.,  1.,   1.]
-a4 = [-1., -1., -1., -1.,  1.,  1.,  1.,   1.]
-a5 = [-1., -1., -1., -1., -1.,  1.,  1.,   1.]
-a6 = [-1., -1., -1., -1., -1., -1.,  1.,   1.]
-a7 = [-1., -1., -1., -1., -1., -1., -1.,   1.]
-a8 = [-1., -1., -1., -1., -1., -1., -1.,   1.]
-a9 = [-1., -1., -1., -1., -1., -1., -1.,  -1.] 
-
-perm0 = set(permutations(a0))
-perm1 = set(permutations(a1))
-perm2 = set(permutations(a2))
-perm3 = set(permutations(a3))
-perm4 = set(permutations(a4))
-perm5 = set(permutations(a5))
-perm6 = set(permutations(a6))
-perm7 = set(permutations(a7))
-perm8 = set(permutations(a8))
-perm9 = set(permutations(a9))
-
-ar0 = np.array(list(perm0))
-ar1 = np.array(list(perm1))
-ar2 = np.array(list(perm2))
-ar3 = np.array(list(perm3))
-ar4 = np.array(list(perm4))
-ar5 = np.array(list(perm5))
-ar6 = np.array(list(perm6))
-ar7 = np.array(list(perm7))
-ar8 = np.array(list(perm8))
-ar9 = np.array(list(perm9))
-
-all_pos = np.concatenate((ar1, ar2, ar3, ar4, ar5, ar6, ar7, ar8, ar9), axis=0)
-
-# Display all possible inputs as greyscale image
-plt.imshow(all_pos,  cmap='gray')
-plt.show()
-
-
-# Takes 3 iterations for all inputs to reach an attractor (2 ~= ln(8))
-for i in range(3):
-    all_pos = np.sign(np.dot(W,all_pos.T).T)
-    
-attractors = np.unique(all_pos, axis=0)
+attractors = np.unique(all_possible_patterns_recall, axis=0)
 print("Number of attractors (assuming convergence): ", attractors.shape[0])
+
+
+all_possible_patterns_recall = hf.degraded_recall_epochs(all_possible_patterns, W, epochs=3)
+
+    
+attractors = np.unique(all_possible_patterns_recall, axis=0)
+print("Number of attractors (assuming convergence): ", attractors.shape[0])
+
+
+
+all_possible_patterns_recall = hf.degraded_recall_first_to_converge(all_possible_patterns, W)
+
+attractors = np.unique(all_possible_patterns_recall, axis=0)
+print("Number of attractors (assuming convergence): ", attractors.shape[0])
+
+
--- hopfield_functions.py
+++ hopfield_functions.py
@@ -5,37 +5,79 @@ Created on Sun Feb  9 12:25:28 2020
 @author: kwc57
 """
 
-import sys
 import math
 import numpy as np 
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-import random 
 
-   
-def weight_calc(data, nrows, disp_W):
-    # Calculate weight matrix
-    W = np.dot(data[:nrows].T,data[:nrows])
-    # Scale by number of points
-    W /= data.shape[1]
+
+#used_data = data[:npatterns]
+
+
+
+def weight_calc(patterns, do_scaling=True, disp_W=False):
+    n_units = patterns.shape[1]
+    
+    # This is the same that summing all the outer products of each pattern with itself
+    W = np.dot(patterns.T,patterns)
+    
+    if do_scaling:
+        W /= n_units
+    np.fill_diagonal(W,0)
     if disp_W:
         # Display weight matrix as greyscale image
         plt.title('Greyscale representation of weight matrix')
-        plt.imshow(W,  cmap='gray')
+        plt.imshow(W,  cmap='jet') 
+        plt.colorbar()
         plt.show()
     return W
 
-def degraded_recall(image_vec, W, epochs, print_step):
+
+
+def degraded_recall_first_to_converge(image_vec_prev, W, print_step=10, plot_patterns=False):
     # Plot input image
-    plt.title("degraded input")
-    img = image_vec.reshape(int(math.sqrt(image_vec.size)),-1)
-    plt.imshow(img, cmap='gray')
-    plt.show()
+    if plot_patterns: 
+        plt.title("degraded input")
+        img = image_vec_prev.reshape(int(math.sqrt(image_vec_prev.size)),-1)
+        plt.imshow(img, cmap='gray')
+        plt.show()
     
+    fixed_point_found=False
+    i=0
+    while ~fixed_point_found:
+    #for i in range(epochs):
+        i += 1
+        image_vec_new = np.sign(np.dot(W,image_vec_prev.T).T)
+        """if (i+1)%print_step == 0:
+                plt.title("update # %i" %(i+1))
+                img = image_vec.reshape(int(math.sqrt(image_vec.size)),-1)
+                plt.imshow(img, cmap='gray')
+                plt.show()  """
+        fixed_point_found = check_fixed_point_found(image_vec_new, image_vec_prev)
+        image_vec_prev = image_vec_new.copy()
+    print("Number of epochs needed to find a fixed point " + str(i))
+    return image_vec_new 
+
+
+
+
+
+
+def check_fixed_point_found(patterns_new, patterns_prev): 
+    fixed_points_in_patterns = np.all(patterns_new == patterns_prev, axis = 1)
+    fixed_point_found = np.any(fixed_points_in_patterns)
+    return fixed_point_found
+
+
+
+
+
+def degraded_recall_epochs(image_vec_prev, W, epochs=10):
     for i in range(epochs):
-        image_vec = np.sign(np.dot(W,image_vec.T).T)
-        if (i+1)%print_step == 0:
-            plt.title("update # %i" %(i+1))
-            img = image_vec.reshape(int(math.sqrt(image_vec.size)),-1)
-            plt.imshow(img, cmap='gray')
-            plt.show()  
+        image_vec_new = np.sign(np.dot(W,image_vec_prev.T).T)
+        """if (i+1)%print_step == 0:
+                plt.title("update # %i" %(i+1))
+                img = image_vec.reshape(int(math.sqrt(image_vec.size)),-1)
+                plt.imshow(img, cmap='gray')
+                plt.show()  """
+        image_vec_prev = image_vec_new.copy()
+    return image_vec_new 
